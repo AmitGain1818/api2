6f3b6ec01e423a76449dcf238e0bf9e182068f69
amitgain1818 <sampamondal75@gmail.com>
2021-12-30 16:38:01 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
@@ -21,7 +20,6 @@ class _MyAppState extends State<MyApp> {
 
   fetchData() async {
     http.Response response;
-
     response = await http.get(
       Uri.parse('https://reqres.in/api/users?page=2'),
     );
@@ -43,95 +41,113 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
           appBar: AppBar(
-            title: Text('List View map'),
+            backgroundColor: Colors.lightGreen,
+            title: const Text('List View API'),
           ),
           drawer: Drawer(
             backgroundColor: Colors.green[100],
             elevation: 6.0,
             child: ListView(
               children: [
-                DrawerHeader(
+                const DrawerHeader(
                   decoration: BoxDecoration(
                     gradient: LinearGradient(
-                        begin: Alignment.bottomLeft,
-                        end: Alignment.topRight,
-                        colors: [
-                          Colors.greenAccent,
-                          Colors.lightGreen,
-                        ]),
+                      begin: Alignment.bottomLeft,
+                      end: Alignment.topRight,
+                      colors: [
+                        Colors.greenAccent,
+                        Colors.lightGreen,
+                      ],
+                    ),
                   ),
-                  child: Text('Drawer Header'),
+                  child: Text(
+                    'Drawer Header',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
                 ),
                 ListTile(
-                  leading: Icon(
+                  leading: const Icon(
                     Icons.person,
                     size: 40,
                     color: Colors.lightGreen,
                   ),
-                  title: Text('person'),
+                  title: const Text('Person'),
                   onTap: () {},
                 ),
                 ListTile(
-                  leading: Icon(
-                    Icons.person,
+                  leading: const Icon(
+                    Icons.inbox,
                     color: Colors.lightGreen,
                   ),
-                  title: Text('person'),
+                  title: const Text('Contact'),
                   onTap: () {},
                 ),
                 ListTile(
-                  leading: Icon(
+                  leading: const Icon(
                     Icons.login,
                     color: Colors.lightGreen,
                   ),
-                  title: Text('person'),
+                  title: const Text('Login'),
                   onTap: () {},
                 ),
                 ListTile(
-                  leading: Icon(
+                  leading: const Icon(
                     Icons.logout,
                     color: Colors.lightGreen,
                   ),
-                  title: Text('person'),
+                  title: const Text('Logout'),
                   onTap: () {},
                 ),
                 ListTile(
-                  leading: Icon(
+                  leading: const Icon(
                     Icons.settings,
                     color: Colors.lightGreen,
                   ),
-                  title: Text('person'),
+                  title: const Text('Setings'),
                   onTap: () {},
                 ),
               ],
             ),
           ),
-          backgroundColor: Colors.green[50],
           body: ListView.builder(
               itemCount: listResponse?.length,
               itemBuilder: (context, index) {
-                return Container(
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          child: Image.network(
-                            listResponse?[index]['avatar'],
-                            height: 300,
-                            width: 300,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
+                return Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Image.network(
+                        listResponse?[index]['avatar'],
+                        height: 200,
+                        width: 200,
+                        fit: BoxFit.cover,
                       ),
-                      Text(listResponse![index]['id'].toString()),
-                      Text(listResponse?[index]['first_name']),
-                      Text(listResponse?[index]['last_name']),
-                      Text(listResponse?[index]['email']),
-                    ],
-                  ),
+                    ),
+                    Text('ID: ${listResponse![index]['id'].toString()}'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          listResponse?[index]['first_name'],
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(
+                          width: 2,
+                        ),
+                        Text(
+                          listResponse?[index]['last_name'],
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                    Text('Email: ${listResponse?[index]['email']}'),
+                  ],
                 );
               })),
     );
